94fceb2b837f4489bca408bb3032d184207ccb3c
unknown <jung_woo@live.com>
2017-10-25 20:21:41 -0500

    Day 8 Woo Jung

--- lib.js
+++ lib.js
@@ -23,10 +23,17 @@ const listedPrice =
  * transform carts into an array of { customer, total }
  */
 const calculateTotals =
-  listings =>
-    carts => {
-      // TODO
-    }
+listings =>
+  carts =>
+  carts.map(cart => ({
+      customer: cart.customer[0].toUpperCase() + cart.customer.slice(1),
+      total: cart.items.reduce((acc, next) =>
+        acc + listings.filter(item =>
+          item.name === next).map(item =>
+            item.price).reduce((acc, next) =>
+              acc + next, 0), 0)
+  })
+  )
 
 module.exports = {
   listing,
